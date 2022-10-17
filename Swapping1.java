05d2d09f4288f470180173ca874886197408d409
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-10-17 20:37:30 +0530

    Add files via upload

---
+++ Swapping1.java
@@ -0,0 +1,14 @@
+// wap to swap 2 numbers without using third variable
+class Swapping
+{
+public static void main(String[] args)
+{
+	int a=10;
+	int b=20;
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println(a);
+	System.out.println(b);
+}
+}
