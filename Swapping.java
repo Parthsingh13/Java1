e9a7fe5e9250a9df6577a9add6d8c2328c9be552
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-10-17 20:25:45 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,17 @@
+//wap to swap 2 variables using third variable
+class Test
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		int c;
+		 c=b;
+		 b=a;
+		 a=c;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
+
+
