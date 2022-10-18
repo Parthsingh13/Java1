b8d1a50a586dbcf0e518cb16cac6c27332b65cf8
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-10-18 22:01:13 +0530

    Add files via upload

---
+++ Repetition.java
@@ -0,0 +1,11 @@
+// Repetition of any string ,number etc
+class Test
+{
+	public static void main(String[] args)
+	{
+		for(int i=0; i<=5; i++)
+		{
+			System.out.println("Parth Singh");
+		}
+	}
+}
