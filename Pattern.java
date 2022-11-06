80549e4796de532e508e28bf56bd5e7a92c2d3a2
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-11-06 23:47:39 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Test {
+    public static void main(String[] args) {
+        System.out.printf("Enter the number of rows :");
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        System.out.printf("The pattern is as follows :\n");
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.printf(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.printf("%d",j);
+            }
+            System.out.printf("\n");
+        }
+    }
+}
