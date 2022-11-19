eab901762ec3d1680403306120ad79dcfd0c3c0f
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-11-19 20:32:46 +0530

    Add files via upload

---
+++ Program3.java
@@ -0,0 +1,27 @@
+// Middle string
+import java.util.Scanner;
+class Test{
+  public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Input a string: ");
+        String str = sc.nextLine();
+        System.out.print("The middle character in the string: " + middle(str)+"\n");
+    }
+ public static String middle(String str)
+    {
+        int position;
+        int length;
+        if (str.length() % 2 == 0)
+        {
+            position = str.length() / 2 - 1;
+            length = 2;
+        }
+        else
+        {
+            position = str.length() / 2;
+            length = 1;
+        }
+        return str.substring(position, position + length);
+    }
+}
