3ed6b1c0dcbc3c485e9a3cd673745f8b1cec584c
Parthsingh13 <100417757+Parthsingh13@users.noreply.github.com>
2022-11-22 21:52:44 +0530

    Add files via upload

---
+++ program6.java
@@ -0,0 +1,24 @@
+// sum of digit in an integer
+import java.util.Scanner;
+class Test{
+
+  public static void main(String[] args)
+    {
+      Scanner sc= new Scanner(System.in);
+      System.out.print("Input an integer: ");
+      int digits = sc.nextInt();
+	  System.out.println("The sum is " + sumDigits(digits));
+    }
+
+ public static int sumDigits(long n) {
+		int result = 0;
+		
+		while(n > 0) {
+			result += n % 10;
+			n /= 10;
+		}
+		
+		return result;
+	}
+	
+ }
